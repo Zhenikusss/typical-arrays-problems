5ffff7e5e95b21c6ae6cc9cc0825ca46278c01f3
Yauheni Kozak <zhenikusss@gmail.com>
2020-03-03 16:00:24 +0300

    feat: implement function typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!(Array.isArray(array)) || (array.length == 0)) {
+        return 0;
+    } else {
+        return Math.min(...array);
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!(Array.isArray(array)) || (array.length == 0)) {
+        return 0;
+    } else {
+        return Math.max(...array);
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!(Array.isArray(array)) || (array.length == 0)) {
+        return 0;
+    } else {
+        let avg = 0;
+        for (let i = 0; i < array.length; i++) {
+            avg += array[i];
+        }
+        avg = avg / array.length;
+        return avg; 
+    }
 }
